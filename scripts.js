3a6c238749d4a3c3f219c5e7ccaf2c17706fa7b8
guicarvv <guisouza2103@yahoo.comv>
2024-03-18 09:56:01 -0300

    formulario

--- scripts.js
+++ scripts.js
@@ -2,7 +2,22 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click",alertar);
 var nome = document.getElementById("nome");
 
+var saida = document.getElementById("saida-de-dados");
+
+var email = document.getElementById("email");
+
+var numero = document.getElementById("telefone");
+
+var numero = document.getElementById("CEP");
+
+var numero = document.getElementById("logradouro");
+
+var numero = document.getElementById("CPF");
+
+
 function alertar(){
-    alert(nome.value + " " + "clicou no botão!!!");
+   // alert(nome.value + " " + "clicou no botão!!!");
+saida.innerText = "Nome: " + "  " + nome.value +
+                "\n E-mail: " + " " + email.value;
 
 }
